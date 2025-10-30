ed79f8e73a610ecf1631d8a5cb619afa4c16b364
DaveBalda <davide.baldacini@gmail.com>
2025-10-30 13:28:52 +0100

    Initial commit

---
+++ src/main/java/net/davebalda/morestuff/MoreStuff.java
@@ -0,0 +1,21 @@
+package net.davebalda.morestuff;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreStuff implements ModInitializer {
+	public static final String MOD_ID = "morestuff";
+
+    /**
+     * Logger per la scrittura nel terminale di messaggi in fase di esecuzione
+     * della mod.
+     */
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// Empty
+	}
+}
---
+++ src/main/java/net/davebalda/morestuff/MoreStuffClient.java
@@ -0,0 +1,11 @@
+package net.davebalda.morestuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreStuffClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
